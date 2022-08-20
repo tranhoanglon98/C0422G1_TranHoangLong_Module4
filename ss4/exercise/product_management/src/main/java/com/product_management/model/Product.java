2a444c9aa019dae05038237fd234dc9cd60a66ff
tranhoanglon98 <you@example.com>
2022-08-20 11:19:41 +0700

    ss4

--- ss4/exercise/product_management/src/main/java/com/product_management/model/Product.java
+++ ss4/exercise/product_management/src/main/java/com/product_management/model/Product.java
@@ -1,11 +1,9 @@
 package com.product_management.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
